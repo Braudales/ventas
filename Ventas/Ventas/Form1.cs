42374ee5232133e69cc6a770afeacd728cd69461
brayan <Brayanmejia28@gmail.com>
2021-09-12 21:11:20 -0600

    Agregando

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,9 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");    
+            MessageBox.Show("Hola Mundo.");
         }
     }
 }
+    
+
