63e122cfb4d331f639a67c127102024b42016f8d
Brayanmejia28@gmail.com <Brayanmejia28@gmail.com>
2021-09-12 21:44:29 -0600

    dsds

--- Ventas/Ventas/Form1.Designer.cs
+++ Ventas/Ventas/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Procesar";
+            this.button1.Text = "Procesar.";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
